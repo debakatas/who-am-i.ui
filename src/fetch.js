3f93d65942b1be40baf0bff13cb758f213f45c41
Nicolas M. Pardo <nicolasm@nikodermus.media>
2020-08-08 16:11:04 -0500

    Update live workshop

--- src/fetch.js
+++ src/fetch.js
@@ -1,4 +1,4 @@
-const endpoint = `http://localhost:9000`;
+const endpoint = process.env.REACT_APP_BACKEND || `http://localhost:9000`;
 
 export const doGet = async (path) => {
     const res = await fetch(`${endpoint}/${path}`);
